f0456e801a3b48b12648c4069e175e2a8eb0f4a9
glenchiridza <glenchiridz@gmail.com>
2021-11-30 00:07:37 +0200

    add room db

--- app/src/main/java/com/glencconnnect/usingroomdbjava/room_models/WordDao.java
+++ app/src/main/java/com/glencconnnect/usingroomdbjava/room_models/WordDao.java
@@ -4,8 +4,27 @@
 
 package com.glencconnnect.usingroomdbjava.room_models;
 
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
+
 @Dao
 public interface WordDao {
+
+    //on conflict strategy ignores a new word if it's exactly the same as one already in the list.
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Word word);
+
+    @Query("DELETE FROM word_table")
+    void deleteAll();
+
+    @Query("SELECT * FROM word_table ORDER BY word ASC")
+    LiveData<List<Word>> getAlphabetizedWords();
+
 }
 
 
